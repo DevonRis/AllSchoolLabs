6c495549d31ea251aa8b33237c10fe75ad94bcb3
Devon Rismay <drismay@energyogre.com>
2025-06-15 14:54:33 -0500

    Updated Cpp File

--- Cpp Programming Files/test.cpp
+++ Cpp Programming Files/test.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 #include <vector>
 #include <unordered_map>
+using namespace std;
 
 
 int main() {
-    std::vector<int> birthDayKeys = {1995, 2000, 1986, 1942, 1959, 1990, 2005};
-    std::vector<std::string> employees = {"Jane", "John", "James", "Jack", "Jenny", "Devon", "Cory"};
+    vector<int> birthDayKeys = {1995, 2000, 1986, 1942, 1959, 1990, 2005};
+    vector<string> employees = {"Jane", "John", "James", "Jack", "Jenny", "Devon", "Cory"};
 
-    std::unordered_map<int, std::string> employeeInfo;
+    unordered_map<int, string> employeeInfo;
 
     if (birthDayKeys.size() == employees.size())
     {
@@ -18,12 +19,12 @@ int main() {
 
         for (const auto& detail : employeeInfo)
         {
-            std::cout << "Birthdays: " << detail.first << ", Names: " << detail.second << std::endl;
+            cout << "Birthdays: " << detail.first << ", Names: " << detail.second << endl;
         }
     }
     else
     {
-        std::cout << "Error: Keys and values must be the same." << std::endl;
+        cout << "Error: Keys and values must be the same." << endl;
     }
     return 0;
 }
